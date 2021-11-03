d467dbdef59d6d5a11742ccd0d03f9f7a6404ab5
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-03 12:39:13 -0400

    installed axios

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,12 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Todo from "./components/Todo";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
       <h1 className="todoHdr">todos</h1>
       <Todo />
+      <TodoList />
     </div>
   );
 }
---
+++ Client/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <h1>Here is where the list will go</h1>
+    </div>
+  );
+};
+
+export default TodoList;
