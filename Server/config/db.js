fc61c559b0a559fd96a3b6ded8ee9f72a91318bc
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-03 12:33:44 -0400

    set up db template

---
+++ Server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost/todos", {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ Server/models/Todos.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose;
+
+const TodosSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = Todo = mongoose.model("Todo", TodoSchema);
