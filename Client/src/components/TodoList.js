3cbabcf5bf50da6ab3b6c03c98729c178f462735
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-04 16:38:06 -0400

    cleaned up code not needed in todolist.js

--- Client/src/components/TodoList.js
+++ Client/src/components/TodoList.js
@@ -3,39 +3,6 @@ import axios from "axios";
 import Todo from "./Todo";
 
 const TodoList = ({ list }) => {
-  //   const [list, setList] = useState([]);
-  //   const [complete, setComplete] = useState(false);
-
-  //   useInterval(() => {
-  //     getTasks();
-  //   }, 1000);
-
-  //   function useInterval(callback, delay) {
-  //     const savedCallback = useRef();
-
-  //     useEffect(() => {
-  //       savedCallback.current = callback;
-  //     }, [callback]);
-
-  //     useEffect(() => {
-  //       function tick() {
-  //         savedCallback.current();
-  //       }
-  //       if (delay !== null) {
-  //         let id = setInterval(tick, delay);
-  //         return () => clearInterval(id);
-  //       }
-  //     }, [delay]);
-  //   }
-
-  //   const getTasks = () => {
-  //     const tasks = axios
-  //       .get(`http://localhost:5000/remindme/todos`)
-  //       .then((res) => {
-  //         setList(res.data);
-  //       });
-  //   };
-
   return (
     <div>
       <h1>Goals</h1>
