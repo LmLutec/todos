8f69ec82114c694b12a7a9f02455b993d6104e00
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-04 18:33:21 -0400

    finished css touches

--- Client/src/components/CompletionData.js
+++ Client/src/components/CompletionData.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "../styles/CompletionData.css";
 
 const CompletionData = ({ list }) => {
   const completeCount = list.filter((l) => l.complete === true).length;
   const totalCount = list.length;
 
   return (
-    <div>
-      <h1>Completed Goals</h1>
+    <div className="completionData">
+      {/* <h1>Completed Goals</h1> */}
       <h5>You have completed {completeCount} goals so far!</h5>
       <p>Completion rate: {((completeCount / totalCount) * 100).toFixed(0)}%</p>
     </div>
