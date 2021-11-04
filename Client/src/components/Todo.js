ec17219c9ec8c812530ede2e6e8fe44125ad696e
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-04 15:49:39 -0400

    entire mark complete div is clickable instead of just the icon

--- Client/src/components/Todo.js
+++ Client/src/components/Todo.js
@@ -21,17 +21,19 @@ const Todo = (todo) => {
       <Notifications />
       <h4 className="task">{todo.item.task}</h4>
       <i>{todo.item.dueDate}</i>
-      <div className="complete">
+      <div
+        className="complete"
+        onClick={() => {
+          if (window.confirm("Do you want to mark this goal complete?"))
+            changeComplete();
+        }}
+      >
         <svg
           width="24"
           height="15"
           viewBox="0 0 24 20"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          onClick={() => {
-            if (window.confirm("Do you want to mark this goal complete?"))
-              changeComplete();
-          }}
         >
           <path
             d="M19.77 2.83L21.17 4.23L8.43 16.97L2.83 11.37L4.23 9.97L8.43 14.17L19.77 2.83ZM19.77 0L8.43 11.34L4.23 7.14L0 11.37L8.43 19.8L24 4.23L19.77 0Z"
