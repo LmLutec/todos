97766503653664255178d74fa3e5968005d7bced
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-04 16:46:18 -0400

    added a completion rate and count. troubleshooting math

--- Client/src/components/CompletionData.js
+++ Client/src/components/CompletionData.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-const CompletionData = () => {
+const CompletionData = ({ list }) => {
+  const completeCount = list.filter((l) => l.complete === true).length;
+  const totalCount = list.length;
+
   return (
     <div>
       <h1>Completed Goals</h1>
-      <p>Completion rate: 100%</p>
+      <h5>You have completed {completeCount} goals so far!</h5>
+      <p>Completion rate: {((totalCount / completeCount) * 100).toFixed(3)}</p>
     </div>
   );
 };
