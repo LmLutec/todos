bb9d2a559b2d6e4401675a3ccdcf1525942dd509
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-04 16:36:29 -0400

    created CompletionData component

--- Client/src/components/TodoList.js
+++ Client/src/components/TodoList.js
@@ -2,39 +2,39 @@ import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Todo from "./Todo";
 
-const TodoList = () => {
-  const [list, setList] = useState([]);
-  const [complete, setComplete] = useState(false);
+const TodoList = ({ list }) => {
+  //   const [list, setList] = useState([]);
+  //   const [complete, setComplete] = useState(false);
 
-  useInterval(() => {
-    getTasks();
-  }, 1000);
+  //   useInterval(() => {
+  //     getTasks();
+  //   }, 1000);
 
-  function useInterval(callback, delay) {
-    const savedCallback = useRef();
+  //   function useInterval(callback, delay) {
+  //     const savedCallback = useRef();
 
-    useEffect(() => {
-      savedCallback.current = callback;
-    }, [callback]);
+  //     useEffect(() => {
+  //       savedCallback.current = callback;
+  //     }, [callback]);
 
-    useEffect(() => {
-      function tick() {
-        savedCallback.current();
-      }
-      if (delay !== null) {
-        let id = setInterval(tick, delay);
-        return () => clearInterval(id);
-      }
-    }, [delay]);
-  }
+  //     useEffect(() => {
+  //       function tick() {
+  //         savedCallback.current();
+  //       }
+  //       if (delay !== null) {
+  //         let id = setInterval(tick, delay);
+  //         return () => clearInterval(id);
+  //       }
+  //     }, [delay]);
+  //   }
 
-  const getTasks = () => {
-    const tasks = axios
-      .get(`http://localhost:5000/remindme/todos`)
-      .then((res) => {
-        setList(res.data);
-      });
-  };
+  //   const getTasks = () => {
+  //     const tasks = axios
+  //       .get(`http://localhost:5000/remindme/todos`)
+  //       .then((res) => {
+  //         setList(res.data);
+  //       });
+  //   };
 
   return (
     <div>
